2c6d9e192943255f0c342f1a5f90e013d0255646
Django project <dd@gmail.com>
2024-03-20 23:06:35 +0500

    Postgre connected

--- goods/models.py
+++ goods/models.py
@@ -26,7 +26,7 @@ class Categories(models.Model):
 
 
 class Products(models.Model):
-    name=models.CharField(max_length=150, unique=True, verbose_name="Name")
+    name=models.CharField(max_length=150,  verbose_name="Name")
     slug=models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
     description=models.TextField(blank=True, null=True, verbose_name="Description")
     image=models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Picture')
--- goods/utils.py
+++ goods/utils.py
@@ -1,4 +1,7 @@
 import keyword
+from re import search
+from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchQuery, SearchRank
 from django.db.models import Q
 from goods.models import Products
 
@@ -6,10 +9,16 @@ from goods.models import Products
 def q_search(query):
     if query.isdigit() and len(query)<=5:
         return Products.objects.filter(id=int(query))
-    keyword=[word for word in query.split() if len(word)>2]
-    q_object=Q()
-    for tokn in keyword:
-        q_object |=Q(description__icontains=tokn)
-        q_object |=Q(name__icontains=tokn)
+    
+    vector = SearchVector("name", "description")
+    query=SearchQuery(query)
+    
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    return Products.objects.filter(q_object)
+    # keyword=[word for word in query.split() if len(word)>2]
+    # q_object=Q()
+    # for tokn in keyword:
+    #     q_object |=Q(description__icontains=tokn)
+    #     q_object |=Q(name__icontains=tokn)
+
+    # return Products.objects.filter(q_object)
