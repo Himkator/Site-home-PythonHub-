42864dbb36062c672793300048640253351d6f69
Django project <dd@gmail.com>
2024-03-21 21:42:49 +0500

    profile and messages

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,8 @@
 from dataclasses import field
 from logging import PlaceHolder
+from pyexpat import model
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
@@ -26,3 +27,20 @@ class UserRegistrationForm(UserCreationForm):
     email=forms.CharField()
     password1=forms.CharField()
     password2=forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model=User
+        fields=(
+            'image',
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+        )
+    image=forms.ImageField(required=False)
+    first_name=forms.CharField()
+    last_name=forms.CharField()
+    username=forms.CharField()
+    email=forms.CharField()
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.http.response import HttpResponseBase
 from django.shortcuts import redirect, render
 from django.urls import reverse
-
+from django.contrib.auth.decorators import login_required
 from users.forms import *
 
 # Create your views here.
@@ -16,6 +16,7 @@ def login(request):
             user=auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"{username}, Вы успешно вошли")
                 return HttpResponseRedirect(reverse('main:main'))
     else:
         form=UserLoginForm()
@@ -32,6 +33,7 @@ def registration(request):
         if form.is_valid():
             user=form.save()
             auth.login(request, user)
+            messages.success(request, f"{user.username}, Вы успешно вошли")
             return HttpResponseRedirect(reverse('main:main'))
     else:
         form=UserRegistrationForm()
@@ -43,14 +45,25 @@ def registration(request):
     }
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
+    if request.method=='POST':
+        form=ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профайл успешно изменен")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form=ProfileForm(instance=request.user)
+
     context={
         'title':'Home-Кабинет',
+        'form':form,
     }
     return render(request, 'users/profile.html', context)
 
-
+@login_required
 def logout(request):
     auth.logout(request)
+    messages.success(request, f"{request.user.username}, Вы успешно вышли")
     return redirect(reverse('main:main'))
